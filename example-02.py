cc868ad58fb15937bdc973e20a336cd31afeaa65
Alexander Palmer <apalmer@zbw.ch>
2025-04-17 14:24:53 +0200

    kleine Anpassungen

--- example-02.py
+++ example-02.py
@@ -1,11 +1,6 @@
 import numpy as np
 import pandas as pd
 
-daten = {
-    'Name': ['Max', 'Peter', 'Julia', 'Andrea'],
-    'Alter': [25, 30, 35, 40],
-    'Stadt': ['Berlin', 'Hamburg', 'München', 'Köln']
-}
-
-df = pd.DataFrame(daten)
-print(df)
+dates = pd.date_range('20230101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+df.plot()
